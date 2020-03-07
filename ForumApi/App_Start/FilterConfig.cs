509ccda45716b92f2ecc5dcf2a34883d9c604e78
Tuzilow <1176281967@qq.com>
2020-03-07 11:17:38 +0800

    添加项目文件。

---
+++ ForumApi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ForumApi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
